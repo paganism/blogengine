7d9542e59c2ee722deb4bcfc8c1d59d1bc250322
yuriy_beloff <paganismrus@gmail.com>
2018-11-21 20:50:40 +0300

    added pagination

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -10,11 +10,31 @@ from .models import Post, Tag
 from .utils import ObjectDetailMixin, ObjectUpdateMixin, ObjectCreateMixin, ObjectDeleteMixin
 from .forms import TagForm, PostForm
 
+from django.core.paginator import Paginator
 
-# Create your views here.
+
 def posts_list(request):
-    posts = Post.objects.all().order_by('-date_pub',)
-    return render(request, 'blog/index.html', context={'posts':posts})
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+    is_paginated = page.has_other_pages()
+    
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+    context = {
+        'page_object':page,
+        'is_paginated': is_paginated,
+        'prev_url': prev_url,
+        'next_url': next_url
+        }
+    return render(request, 'blog/index.html', context=context)
 
 
 # def post_detail(request, slug):
@@ -134,4 +154,3 @@ class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     template = 'blog/post_delete_form.html'
     redirect_url = 'posts_list_url'
     raise_exception = True
-    
